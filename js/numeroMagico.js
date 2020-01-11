3a1a4906e7fca325e69804a317775ae68e61996f
Gabriel Cano <gabrielcano.86@gmail.com>
2020-01-11 12:57:43 -0300

    se termina el java script del juego

---
+++ js/numeroMagico.js
@@ -0,0 +1,58 @@
+function numAleat() {
+    return Math.round(Math.random() * 100); // se indica un valor aleatorio entre 1 a 100
+}
+
+
+
+function controlValor(numctrl, resultado) {
+
+
+    if (numctrl == resultado) {
+        return true
+    }
+
+    return false
+
+
+}
+
+
+
+function inicioJuego(num, i, resultado) {
+
+
+
+    if (i > 1) {
+
+        if (controlValor(num, resultado)) {
+           return alert(`Adivinaste`);
+            
+        }
+
+        else {
+            
+            i--;
+            alert(`No adivinaste, te quedan ${i}oportunidades`);
+            
+            let valor = prompt('adivine el numero de 1 a 100:');
+            inicioJuego(valor, i,resultado);
+
+        }
+    } else {
+        alert(`PERDISTE`);
+    }
+
+}
+
+let i = 3;
+let resultado = numAleat();
+console.log(resultado); // se muestra el valor aleatorio
+
+let valor = prompt('adivine el numero de 1 a 10:');
+
+inicioJuego(valor, i, resultado) 
+
+
+
+
+
