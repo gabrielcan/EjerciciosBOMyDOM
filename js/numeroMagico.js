c85adefd7cc215170aeacf0b76936c70398eab99
Gabriel Cano <gabrielcano.86@gmail.com>
2020-01-12 14:04:39 -0300

    numero magico hecho falta validacion con try catch

--- js/numeroMagico.js
+++ js/numeroMagico.js
@@ -1,66 +1,122 @@
 function numAleat() {
-    document.getElementById('valor_oprt').innerHTML=3;
-    
-    return Math.round(Math.random() * 100); // se indica un valor aleatorio entre 1 a 100
+    document.getElementById('lab_valor_oprt').innerHTML = 3;
+
+    return Math.round(Math.random() * 10); // se indica un valor aleatorio entre 1 a 100
 }
 
 
 
 function controlValor(numctrl, resultado) {
 
-
     if (numctrl == resultado) {
         return true
     }
-
     return false
 
-
 }
+
+function espera(){
+    setTimeout(function () {
+         window.location = "numeroMagico.html" 
+     }, 5000);
+
+ }  
 
 
 
 function inicioJuego(resultado) {
-let band=0;
-let num = document.getElementById('num').value;
-let oport= document.getElementById('valor_oprt').innerHTML;
-console.log(oport);
+    let band = 0;
+    let num = document.getElementById('num').value;
+    let oport = document.getElementById('lab_valor_oprt').innerHTML;
+ //   console.log(oport);
+
+if(controlValor(num)){
+    if (controlValor(num, resultado)) {
 
 
-    if(controlValor(num, resultado)){
+        document.getElementById('valor_oprt').style.display = 'none';
+        document.getElementById('resultado').style.display = 'flex !important';
+        document.getElementById('cont_form_adivinar').style.display = 'none';
 
-        
-        document.getElementById('valor_oprt').innerHTML=3;
-        document.getElementById('resultado').style.display='flex !important';
-        
         document.getElementById("num").disabled = true;
         document.getElementById("adivinar").disabled = true;
-        document.getElementById('valor_oprt').innerHTML='ADIVINASTE!!';
+        document.getElementById('emoticon2').style.display = 'flex';
+        document.getElementById("txt_result").style.display = 'flex';
+        document.getElementById("txt_result").style.color='black';
+        document.getElementById('txt_result').style.backgroundColor='green'
+        document.getElementById('txt_result').innerHTML = 'ADIVINASTE!!';
 
-    } else{
-       
-       alert('No adivinas')
-        document.getElementById('valor_oprt').innerHTML=  document.getElementById('valor_oprt').innerHTML-1;
-        oport= document.getElementById('valor_oprt').innerHTML;
+  espera();
 
-        console.log(oport);    
-        if(oport==0){
+    } else {
+        document.getElementById('num').focus();
+        document.getElementById('num').value='';
+        document.getElementById("txt_result").style.display = 'flex';
+        document.getElementById('txt_result').innerHTML = "No adivinaste, intentalo de nuevo"
+        document.getElementById('lab_valor_oprt').innerHTML = document.getElementById('lab_valor_oprt').innerHTML - 1;
+        oport = document.getElementById('lab_valor_oprt').innerHTML;
+        
+        switch (parseInt(oport)) {
+            case 1:
+                document.getElementById("txt_result").style.width='100%';
+                document.getElementById("txt_result").style.color='black';
+                document.getElementById('txt_result').style.backgroundColor='#ffc107b0'
 
+                break;
+            case 2:
+                document.getElementById("txt_result").style.width='90%';
+                document.getElementById("txt_result").style.color='#ffeb3bdb';
+                document.getElementById("txt_result").style.backgroundColor='black';
+                break;
+
+            default:
+           
+                break;
+        }
+
+      //  console.log(oport);
+
+        if (oport == 0) {
+            document.getElementById('cont_form_adivinar').style.display = 'none';
+            document.getElementById('valor_oprt').style.display = 'none';
             document.getElementById("num").disabled = true;
             document.getElementById("adivinar").disabled = true;
+            document.getElementById('emoticon').style.display = 'flex';
+            document.getElementById("txt_result").style.display = 'flex';
+            document.getElementById('txt_result').style.backgroundColor='Red'
+           
+            document.getElementById('txt_result').innerHTML = "PERDISTE"
+            espera();
 
-            return document.getElementById('valor_oprt').innerHTML="PERDISTE"
-            
         }
     }
+
+} else{
+    alert('error datos input')
+}
+
+}
+
+function ctrl_input(valor){
+
+try{
+
+if(valor=="") throw "empty";
+if(isNaN(valor)) throw "No es un número";
+valor=Number(valor);
+if(valor<=10) throw "tiene que ser un valor menor o igual a 10";
+
+} catch(err){
+    document.getElementById('txt_result').innerHTML = "valor no valido,ingresa otro"
+    console.log(err);
+}    
 
 }
 
 
 
-
 let resultado = numAleat();
-let oport=3;
+let oport = 3;
 console.log(resultado); // se muestra el valor aleator
 
 
@@ -74,7 +130,7 @@ console.log(resultado); // se muestra el valor aleator
 // }
 //  let numero= get_num();
 //  inicioJuego(numero, i, resultado);
- 
+
 
 
 
