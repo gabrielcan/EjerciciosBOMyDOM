5001eb45d29501b54fdcfab0b076a24b5f1e2cf7
Gabriel Cano <gabrielcano.86@gmail.com>
2020-01-12 22:33:49 -0300

    se termina juego

--- js/numeroMagico.js
+++ js/numeroMagico.js
@@ -15,105 +15,123 @@ function controlValor(numctrl, resultado) {
 
 }
 
-function espera(){
+function espera() {
     setTimeout(function () {
-         window.location = "numeroMagico.html" 
-     }, 5000);
+        window.location = "numeroMagico.html"
+    }, 5000);
 
- }  
+}
 
 
 
 function inicioJuego(resultado) {
-    let band = 0;
+    
     let num = document.getElementById('num').value;
     let oport = document.getElementById('lab_valor_oprt').innerHTML;
- //   console.log(oport);
+    //   console.log(oport);
 
-if(controlValor(num)){
-    if (controlValor(num, resultado)) {
+    //console.log(ctrl_input(num));
+
+    if (ctrl_input(num) != false) {
+
+        document.getElementById("txt_result").style.display = 'none';
+
+        if (controlValor(num, resultado)) {
 
 
-        document.getElementById('valor_oprt').style.display = 'none';
-        document.getElementById('resultado').style.display = 'flex !important';
-        document.getElementById('cont_form_adivinar').style.display = 'none';
-
-        document.getElementById("num").disabled = true;
-        document.getElementById("adivinar").disabled = true;
-        document.getElementById('emoticon2').style.display = 'flex';
-        document.getElementById("txt_result").style.display = 'flex';
-        document.getElementById("txt_result").style.color='black';
-        document.getElementById('txt_result').style.backgroundColor='green'
-        document.getElementById('txt_result').innerHTML = 'ADIVINASTE!!';
-
-  espera();
-
-    } else {
-        document.getElementById('num').focus();
-        document.getElementById('num').value='';
-        document.getElementById("txt_result").style.display = 'flex';
-        document.getElementById('txt_result').innerHTML = "No adivinaste, intentalo de nuevo"
-        document.getElementById('lab_valor_oprt').innerHTML = document.getElementById('lab_valor_oprt').innerHTML - 1;
-        oport = document.getElementById('lab_valor_oprt').innerHTML;
-        
-        switch (parseInt(oport)) {
-            case 1:
-                document.getElementById("txt_result").style.width='100%';
-                document.getElementById("txt_result").style.color='black';
-                document.getElementById('txt_result').style.backgroundColor='#ffc107b0'
-
-                break;
-            case 2:
-                document.getElementById("txt_result").style.width='90%';
-                document.getElementById("txt_result").style.color='#ffeb3bdb';
-                document.getElementById("txt_result").style.backgroundColor='black';
-                break;
-
-            default:
-           
-                break;
-        }
-
-      //  console.log(oport);
-
-        if (oport == 0) {
-            document.getElementById('cont_form_adivinar').style.display = 'none';
             document.getElementById('valor_oprt').style.display = 'none';
+            document.getElementById('resultado').style.display = 'flex !important';
+            document.getElementById('cont_form_adivinar').style.display = 'none';
+
             document.getElementById("num").disabled = true;
             document.getElementById("adivinar").disabled = true;
-            document.getElementById('emoticon').style.display = 'flex';
+            document.getElementById('emoticon2').style.display = 'flex';
+            document.getElementById('resultado').style.width = '66%';
             document.getElementById("txt_result").style.display = 'flex';
-            document.getElementById('txt_result').style.backgroundColor='Red'
-           
-            document.getElementById('txt_result').innerHTML = "PERDISTE"
+            document.getElementById("txt_result").style.color = 'black';
+            document.getElementById('txt_result').style.backgroundColor = 'green'
+            document.getElementById('txt_result').innerHTML = 'ADIVINASTE!!';
+
             espera();
 
+        } else {
+            document.getElementById('num').focus();
+            limpiar()
+            document.getElementById("valor_oprt").style.display = 'flex';
+            document.getElementById("txt_result").style.display = 'flex';
+            document.getElementById('txt_result').innerHTML = "No adivinaste, intentalo de nuevo"
+            document.getElementById('lab_valor_oprt').innerHTML = document.getElementById('lab_valor_oprt').innerHTML - 1;
+            oport = document.getElementById('lab_valor_oprt').innerHTML;
+
+            switch (parseInt(oport)) {
+                case 1:
+                    document.getElementById("txt_result").style.width = '100%';
+                    document.getElementById("txt_result").style.color = 'black';
+                    document.getElementById('txt_result').style.backgroundColor = '#546e7a'
+
+                    break;
+                case 2:
+                    document.getElementById("txt_result").style.width = '90%';
+                    document.getElementById("txt_result").style.color = '#ffeb3bdb';
+                    document.getElementById("txt_result").style.backgroundColor = 'black';
+                    break;
+
+                default:
+
+                    break;
+            }
+
+            //  console.log(oport);
+
+            if (oport == 0) {
+                document.getElementById('cont_form_adivinar').style.display = 'none';
+                document.getElementById('valor_oprt').style.display = 'none';
+                document.getElementById("num").disabled = true;
+                document.getElementById("adivinar").disabled = true;
+                document.getElementById('emoticon').style.display = 'flex';
+                document.getElementById("txt_result").style.display = 'flex';
+                document.getElementById('txt_result').style.backgroundColor = 'Red'
+                document.getElementById('resultado').style.width = '66%';
+                document.getElementById('txt_result').innerHTML = "PERDISTE"
+                espera();
+
+            }
         }
+
+    } else {
+
+        limpiar()
+
     }
 
-} else{
-    alert('error datos input')
 }
 
-}
+function ctrl_input(valor) {
+  
+    try {
 
-function ctrl_input(valor){
+        if (valor=="") throw console.log('estoy en null'), document.getElementById("txt_result").style.display = 'flex',document.getElementById('txt_result').style.backgroundColor = 'Orange',document.getElementById('txt_result').style.color= 'black', document.getElementById('txt_result').innerHTML = 'No puedes enviar valores vacios';
+        valor = Number(valor);
+        if (valor > 10) throw document.getElementById("txt_result").style.display = 'flex',document.getElementById('txt_result').style.backgroundColor = 'Orange',document.getElementById('txt_result').style.color= 'black', document.getElementById('txt_result').innerHTML = 'Ingresa valores menores o iguales a 10';
 
-try{
+    } catch (err) {
 
-if(valor=="") throw "empty";
-if(isNaN(valor)) throw "No es un número";
-valor=Number(valor);
-if(valor<=10) throw "tiene que ser un valor menor o igual a 10";
+        return false;
+    }
 
-} catch(err){
-    document.getElementById('txt_result').innerHTML = "valor no valido,ingresa otro"
-    console.log(err);
-}    
 
 }
 
+function limpiar() {
+    document.getElementById('num').value = '';
+}
 
+function entrar(){
+
+    document.getElementById("cont_input").style.display = 'flex';
+    document.getElementById("cont_button_inicio").style.display = 'none';
+
+}
 
 let resultado = numAleat();
 let oport = 3;
